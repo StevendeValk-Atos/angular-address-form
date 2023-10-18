3ec574d6c07081f8230bc02fc04d26f1448fbdb7
Steven de Valk <steven.devalk@atos.net>
2023-10-18 16:42:43 +0200

    Added form-input and form-label atom base files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormInputComponent } from './atoms/form-input/form-input.component';
+import { FormLabelComponent } from './atoms/form-label/form-label.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormInputComponent,
+    FormLabelComponent
   ],
   imports: [
     BrowserModule
