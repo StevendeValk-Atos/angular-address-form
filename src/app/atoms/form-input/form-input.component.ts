8dab10486520303aff1225627f71c02a211bc8de
Steven de Valk <steven.devalk@atos.net>
2023-10-20 13:31:58 +0200

    Imported FormsModule in app.module.ts, added base for value binding in form-input atom

--- src/app/atoms/form-input/form-input.component.ts
+++ src/app/atoms/form-input/form-input.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-form-input',
-  templateUrl: './form-input.component.html',
-  styleUrls: ['./form-input.component.scss']
+    selector: "app-form-input",
+    templateUrl: "./form-input.component.html",
+    styleUrls: ["./form-input.component.scss"],
 })
 export class FormInputComponent {
+    value: string = "";
 
+    // src: https://www.angularjswiki.com/angular/ngmodelchange-change-angular/
+    valueChange (value : string) : void {
+        console.log(value);
+    }
 }
